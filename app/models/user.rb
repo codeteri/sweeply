ecdaf2b652cb2197979d1f1aaf83e36b4ec1e0d4
Jack Barham <jack@barhamfamily.com>
2023-11-21 14:28:17 +0200

    user profile page and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  has_many :listings, foreign_key: 'listings_id'
+  has_many :bookings, foreign_key: 'bookings_id'
+
+  validates :name, presence: true
+  validates :phone_number, presence: true, uniqueness: true
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :bio, length: { maximum: 500 }
 end
