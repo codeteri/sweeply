73a4f159f4558e30c645f1037c2be737cdf40634
Alex Wunderlich <wunderlich.ap@gmail.com>
2023-11-21 14:18:31 +0100

    Fixed booking migration timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_150827) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_092256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "phone_number"
-    t.string "email_address"
-    t.text "bio"
+  create_table "bookings", force: :cascade do |t|
+    t.string "size"
+    t.integer "bathrooms"
+    t.string "supplies"
+    t.bigint "user_id", null: false
+    t.bigint "listing_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_bookings_on_listing_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
+
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.string "type"
+    t.string "suburb"
+    t.integer "rate"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bookings", "listings"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "listings", "users"
 end
